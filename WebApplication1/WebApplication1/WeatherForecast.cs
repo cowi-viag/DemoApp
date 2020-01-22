6b49aad9f4272787c7b0d7868cfad4273596d5a1
Vitalijus Agejevas <viag@cowi.com>
2020-01-22 09:46:33 +0200

    Rollback wind properties

--- WebApplication1/WebApplication1/WeatherForecast.cs
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -11,8 +11,5 @@ namespace WebApplication1
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
-
-        public int WindSpeed { get; set; }
-        public string WindDirection { get; set; }
     }
 }
