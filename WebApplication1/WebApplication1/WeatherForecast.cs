5e329b41e400659bf5c59082b010ebf66fae34d1
Vitalijus Agejevas <viag@cowi.com>
2020-01-22 09:00:47 +0200

    Add wind properties

--- WebApplication1/WebApplication1/WeatherForecast.cs
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -11,5 +11,8 @@ namespace WebApplication1
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public int WindSpeed { get; set; }
+        public string WindDirection { get; set; }
     }
 }
